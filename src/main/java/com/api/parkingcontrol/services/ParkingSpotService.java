d7b03102929b4adc0aebca885383d5c566d842e8
Samuelsnd <samueldasilva9211@gmail.com>
2023-11-07 01:38:08 -0300

    adding the pagination for the project

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -3,6 +3,8 @@ package com.api.parkingcontrol.services;
 import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,8 +23,8 @@ public class ParkingSpotService {
     return repository.save(parkingSpotModel);
     }
 
-    public List<ParkingSpotModel> findAll() {
-        return repository.findAll();
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
     }
 
     public Optional<ParkingSpotModel> findById(UUID id) {
